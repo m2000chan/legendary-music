825f2d221100cb58531b36e7cff5181cfc74e240
Max Chan <mchan9@stuy.edu>
2017-10-17 10:29:38 -0400

    Update stuff

--- Music.c
+++ Music.c
@@ -1,12 +1,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-typedef struct song_node{
-
-  char name[256];
-  char artist[256];
-  struct song_node * next; //points to next song/artist
-  
-} song_node;
-
+#include "song_node.h"
--- linklist.c
+++ linklist.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "song_node.h"
 
 void print_list (struct song_node * curr_node) {
   printf("[");
   while (curr_node) { //while there is another node after, print value then proceed to next
-    printf("%d, ", curr_node -> value);
+    printf("Artist: %s\t Song: %s\n", curr_node -> artist, curr_node -> name);
     curr_node = curr_node -> next;
   }
-  printf("%d]\n", curr_node -> value); //print last element
+ printf("Artist: %s\t Song: %s, ", curr_node -> artist, curr_node -> name); //print last element
 }
 
-struct node * insert_front (struct node * exist_node, int val) {
-  struct node *add = malloc (sizeof (struct node)); //allocate memory for new node
-  add -> value = val; //initialize node
+struct song_node * insert_front (struct song_node * exist_node, char * Title, char * Singer) {
+  struct song_node *add = malloc (sizeof (struct song_node)); //allocate memory for new node
+  add -> name = Singer; //initialize node
+    add -> artist = Title; 
   add -> next = exist_node;
   return add;
 }
 
 
-struct node * free_list(struct node * start){
-  struct node * temp;
+struct song_node * free_list(struct song_node * start){
+  struct song_node * temp;
 
   while (start -> next) {
     temp = start;
@@ -31,33 +33,10 @@ struct node * free_list(struct node * start){
   return NULL;
 }
 
-struct node * create_link(val){ //in order to initialize first element
-  struct node * end = malloc(sizeof(struct node));
-  end -> value = val;
+struct song_node * create_link(char * Title, char * Singer){ //in order to initialize first element
+  struct song_node * end = malloc(sizeof(struct song_node));
+  end -> name = Title;
+    end -> artist = Singer;
   end -> next = NULL;
   return end;
-}
-
-
-
-int main() {
-
-  struct node * first = create_link(15);
-  //print_list(&first);
-
-  struct node * zero = insert_front (first, 10);
-
-  printf("\nMy Linked List:\n");
-  print_list(zero); //should be [10, 15]
-
-  printf("\nInserted 5 at the front:\n");
-  struct node * negative = insert_front(zero, 5);
-  print_list(negative); //should be [5, 10, 15]
-
-
-  printf("\nAfter free list, the start pointer points to NULL: %s\n", free_list(negative)? "False":"True");
-
-
-
-  return 0;
 }
