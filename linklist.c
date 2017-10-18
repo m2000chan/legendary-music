e673256f3927bd8ef09a5c05cf9846f16e27bd80
JFreud <jrm.frdnbrg@gmail.com>
2017-10-17 21:57:09 -0400

    added find_song mostly

--- linklist.c
+++ linklist.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "song_node.h"
 
-void print_list (struct song_node * curr_node) {
+void print_list (song_node * curr_node) {
   //printf("[");
   while (curr_node) { //while there is another node after, print value then proceed to next
     printf("Artist: %s\t Song: %s\n", curr_node -> artist, curr_node -> name);
@@ -11,8 +11,8 @@ void print_list (struct song_node * curr_node) {
   }
 }
 
-struct song_node * insert_front (struct song_node * exist_node, char * title, char * singer) {
-  struct song_node *add = malloc (sizeof (struct song_node)); //allocate memory for new node
+song_node * insert_front (song_node * exist_node, char * title, char * singer) {
+  song_node *add = malloc (sizeof (song_node)); //allocate memory for new node
   add -> name = title; //initialize node
   add -> artist = singer;
   add -> next = exist_node;
@@ -20,8 +20,8 @@ struct song_node * insert_front (struct song_node * exist_node, char * title, ch
 }
 
 
-struct song_node * free_list(struct song_node * start){
-  struct song_node * temp;
+song_node * free_list(song_node * start){
+  song_node * temp;
 
   while (start -> next) {
     temp = start;
@@ -32,18 +32,18 @@ struct song_node * free_list(struct song_node * start){
   return NULL;
 }
 
-struct song_node * create_link(char * title, char * singer){ //in order to initialize first element
-  struct song_node * end = malloc(sizeof(struct song_node));
+song_node * create_link(char * title, char * singer){ //in order to initialize first element
+  song_node * end = malloc(sizeof(song_node));
   end -> name = title;
     end -> artist = singer;
   end -> next = NULL;
   return end;
 }
 
-struct song_node * insert_order(song_node * first, char * title, char * singer) {
-  struct song_node *exist_node = first;
-  struct song_node *add = malloc (sizeof( struct song_node));
-  struct song_node *temp = malloc (sizeof( struct song_node));
+song_node * insert_order(song_node * first, char * title, char * singer) {
+  song_node *exist_node = first;
+  song_node *add = malloc (sizeof( song_node));
+  song_node *temp = malloc (sizeof( song_node));
   add -> name = title;
   add -> artist = singer;
   while (exist_node &&
@@ -59,8 +59,20 @@ struct song_node * insert_order(song_node * first, char * title, char * singer) 
     //printf("IT'S A SECRET!");
     return add;
   }
-  return first;//return pointer to beginning of list
+  return first;//otherwise, return pointer to beginning of list
 }
+
+song_node * find_song(song_node * exist_node, char * title, char * singer) {
+  while (exist_node &&
+         (strcmp(singer, exist_node -> artist) < 0 ||
+	       strcmp(title, exist_node -> name) < 0)) {
+           exist_node = exist_node -> next;
+         }
+  return exist_node;//should be NULL if didn't find
+}
+
+
+
 
 int main(){
     song_node * y = create_link("Skyfall", "Adele");
@@ -79,7 +91,11 @@ int main(){
     y = insert_order(y, "Waka Waka", "Shakira");
     print_list(y);
     printf("\n");
-    print_list(y);
+
+    song_node *song_search = find_song(y, "Uptown Girl", "Billy Joel");
+    print_list(song_search);
+    printf("\n");
+
 
     return 0;
 
