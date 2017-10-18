7ac3e3f15b16e16e196574f8c03db2e1e251dd99
Max Chan <mchan9@stuy.edu>
2017-10-18 11:00:38 -0400

    Store changes before class ends

--- Music.c
+++ Music.c
@@ -3,9 +3,18 @@
 #include <string.h>
 #include "linklist.c"
 
+song_node * Alpha[26];
+
 void initialize(){
     
     song_node * Alpha[26]; //Array to store each letter
     
+}
+
+song_node * add_song(char * Title, char * Singer){
+    
+    char x = Singer[0];
+    song_node * p = Alpha[x-97];
+    return insert_order(p, Title, Singer);
 }
 
