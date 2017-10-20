69168c86405150cefaab62e2644ab97d4203b423
JFreud <jrm.frdnbrg@gmail.com>
2017-10-20 10:23:25 -0400

    broke it even more:

--- music.c
+++ music.c
@@ -8,207 +8,217 @@ song_node * song_lists[26];
 song_node * add_song(char * title, char * singer){
 
     char x = singer[0];
-    
+
     if (song_lists[x-97]){ //if the first letter of artist has data in list
-        
+
         song_node * first_song = song_lists[x-97];//subtract value of 'a' to get letter index
-        
+
         song_lists[x-97] = insert_order(first_song, title, singer);
         return song_lists[x-97];
     }
-    
+
     else{
-        
+
         song_node * first_song = create_link(title, singer);
-        
+
         song_lists[x-97] = first_song;
-        
+
         return first_song;
     }
-   
+
 }
 
 void print_library(){
-    
+
     int i = 0;
-    
+
     for (; i < 26; i++){
-    
+
         if (song_lists[i]){
-            
+
             printf("%c:\n", i + 97);
-            
+
         }
-        
+
         print_list(song_lists[i]);
-        
+
     }
-    
+
 }
 
 song_node * song_search(char * title, char * singer){
-    
+
     char x = singer[0];
-    
+
     song_node * search_node = song_lists[x-97];
-    
+
     song_node * returning = find_song(search_node, title, singer);
-    
+
     if (returning == NULL){
-        
+
         printf("\nSONG NOT FOUND\n");
         return NULL;
-        
+
     }
-    
+
     return returning;
 }
 
 
 song_node * artist_search(char * singer){
-    
+
     char x = singer[0];
-    
+
     song_node * search_node = song_lists[x-97];
     //printf("Searching in %c\n", x);
     //print_list(search_node);
-    
+
     song_node * returning = find_artist(search_node, singer);
-    
+
     if (returning == NULL){
-        
+
         printf("\nARTIST NOT FOUND\n");
         return NULL;
-        
+
     }
-    
+
     return returning;
 }
 
 void print_letter(char x){
-    
+
     print_list(song_lists[x-97]);
-    
+
 }
 
 void print_artist(char * singer){
-    
+
     char x = singer[0];
-    
+
     song_node * search_node = song_lists[x-97];
-    
+
     while (search_node -> next &&
            search_node -> artist != singer){
-        
+
         search_node = search_node -> next;
     }
-    
+
     while(search_node -> next &&
           search_node -> artist == singer){
-        
+
         printf("\t%s\t | \t%s\n", search_node -> artist, search_node -> name);
         search_node = search_node -> next;
     }
-    
+
 }
 
-void shuffle(){
-    
-    int length = 0;
-    int i = 0;
-    
-    for (; i < 26; i++){
-        
-        length += get_length(song_lists[i]);
-        
-    }
-    
-    srand(time(NULL));
-    int r = rand();
-    long random = rand() % length - 2;
-    
-    int j = 0;
-    
-    for (; j < random + 1; j ++){
-    
-        int randy = rand() % 26;
-        
-        song_node * rands = random_song(song_lists[randy]);
-        
-        printf("\t%s\t | \t%s\n", rands -> artist, rands -> name);
-    }
-    
+// void shuffle(){
+//
+//     int length = 0;
+//     int i = 0;
+//
+//     for (; i < 26; i++){
+//
+//         length += get_length(song_lists[i]);
+//
+//     }
+//
+//     srand(time(NULL));
+//     int r = rand();
+//     long random = rand() % length - 2;
+//
+//     int j = 0;
+//
+//     for (; j < random + 1; j ++){
+//
+//         int randy = rand() % 26;
+//
+//         song_node * rands = random_song(song_lists[randy]);
+//
+//         printf("\t%s\t | \t%s\n", rands -> artist, rands -> name);
+//     }
+//
+// }
+
+void shuffle(int num_songs){
+  srand(time(NULL));
+  int i;
+  for (i = 0; i < num_songs; i++) {
+    int randart = rand() % 27;
+    song_node * random_pick = random_song(song_lists[randart]);
+    printf("\t%s\t | \t%s\n", random_pick -> artist, random_pick -> name);
+  }
 }
 
 
 int main(){
-    
+
     printf("TESTING ADDING SONGS/PRINT LIBRARY\n\n");
-    
+
     //FOR SOME REASON THE ADD SONGS WORKS DOESN'T WORK IF THE ARTIST DOUBLE UP, BUT IS OKAY WITH A
-    
+
     add_song("skyfall", "adele");
     add_song("hello", "adele");
-    add_song("symphony 40", "mozart"); 
+    add_song("symphony 40", "mozart");
     add_song("ants", "apples");
-    add_song("drive by", "train"); 
+    add_song("drive by", "train");
     add_song("what", "tree");
     add_song("barcelona", "train");
     add_song("orange orange", "chair");
-    add_song("we are", "ke$ha"); 
+    add_song("we are", "ke$ha");
     add_song("sk8tr boy", "lavigne");
     add_song("human", "the killers");
-    add_song("mr. brightside", "the killers"); 
-    
-    print_library();   
-    
+    add_song("mr. brightside", "the killers");
+
+    print_library();
+
     printf("\n============\n\n");
-    
-    
+
+
     printf("TESTING SONG SEARCH: Search Adele - Skyfall:\n\n");
-    
+
     song_node * y = song_search("skyfall", "adele");
-    
+
     print_list(y);
-    
+
     printf("\n============\n\n");
-    
+
     printf("TESTING ARTIST SEARCH: Search ke$ha:\n\n");
-    
+
     y = artist_search("ke$ha");
-    
+
     print_list(y);
-    
+
     printf("\n============\n\n");
-    
+
     printf("TESTING PRINT LETTER: Printing t:\n\n");
-    
+
     print_letter('t');
-    
+
     printf("\n============\n\n");
-    
+
     printf("TESTING PRINT ARTIST: Printing adele:\n\n");
 
     print_artist("adele");
-    
+
     printf("\nPrinting the killers\n\n");
-    
+
     print_artist("the killers");
-    
+
     printf("\n============\n\n");
-    
+
     printf("TESTING SHUFFLE:\n\n");
-    
-    shuffle();
-    
+
+    shuffle(5);
+
     printf("\n============\n\n");
-    
-    
 
 
-    
+
+
+
     /*
-    
+
   printf("\n\nLINKED LIST TESTS\n\n\n");
 
   printf("CREATING FIRST SONG:\n");
@@ -218,7 +228,7 @@ int main(){
   //y = insert_front(y, "Symphony 40", "Mozart");
   //print_list(y);
   printf("\n============\n");
-    
+
 
   printf("TESTING INSERT ORDER:\n");
   y = insert_order(y, "symphony 40.5", "bozart");
