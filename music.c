a8bc1d6d850e93b75eac13c01c5755238573ae4c
Max Chan <mchan9@stuy.edu>
2017-10-20 01:47:43 -0400

    add more stuff

--- music.c
+++ music.c
@@ -5,7 +5,6 @@
 
 song_node * song_lists[26];
 
-<<<<<<< HEAD
 song_node * add_song(char * title, char * singer){
 
     char x = singer[0];
@@ -17,6 +16,7 @@ song_node * add_song(char * title, char * singer){
         return insert_order(first_song, title, singer);
         
     }
+    
     else{
         
         song_node * first_song = create_link(title, singer);
@@ -46,19 +46,128 @@ void print_library(){
     
 }
 
-song_node * song_search(char * Title, char * Singer){
+song_node * song_search(char * title, char * singer){
     
+    char x = singer[0];
+    
+    song_node * search_node = song_lists[x-97];
+    
+    song_node * returning = find_song(search_node, title, singer);
+    
+    if (returning == NULL){
+        
+        printf("\nSONG NOT FOUND\n");
+        return NULL;
+        
+    }
+    
+    return returning;
+}
+
+
+song_node * artist_search(char * singer){
+    
+    char x = singer[0];
+    
+    song_node * search_node = song_lists[x-97];
+    //printf("Searching in %c\n", x);
+    //print_list(search_node);
+    
+    song_node * returning = find_artist(search_node, singer);
+    
+    if (returning == NULL){
+        
+        printf("\nARTIST NOT FOUND\n");
+        return NULL;
+        
+    }
+    
+    return returning;
+}
+
+void print_letter(char x){
+    
+    print_list(song_lists[x-97]);
+    
+}
+
+void print_artist(char * singer){
+    
+    char x = singer[0];
+    
+    song_node * search_node = song_lists[x-97];
+    
+    while (search_node -> next &&
+           search_node -> artist != singer){
+        
+        search_node = search_node -> next;
+    }
+    
+    while(search_node -> next &&
+          search_node -> artist == singer){
+        
+        printf("\t%s\t | \t%s\n", search_node -> artist, search_node -> name);
+        search_node = search_node -> next;
+    }
+    
+    printf("\t%s\t | \t%s\n", search_node -> artist, search_node -> name);
     
 }
 
 
 int main(){
     
-    add_song("skyfall", "adele");
-    add_song("symphony 40", "mozart");
-    add_song("ants", "apples");
+    printf("TESTING ADDING SONGS\n\n");
     
-    print_library();    
+    //FOR SOME REASON THE ADD SONGS WORKS DOESN'T WORK IF THE ARTIST DOUBLE UP, BUT IS OKAY WITH A
+    
+    add_song("skyfall", "adele"); //works
+    add_song("hello", "adele");
+    add_song("symphony 40", "mozart"); //works
+    add_song("ants", "apples"); //works 
+    add_song("drive by", "train"); //works 
+    add_song("what", "tree"); //works
+    add_song("barcelona", "train"); //DOESNT work
+    add_song("orange orange", "chair"); //works
+    add_song("we are", "ke$ha"); //works
+    add_song("sk8tr boy", "lavigne"); //works
+    add_song("human", "the killers"); //DOESN'T work
+    add_song("mr. brightside", "the killers"); //DOESN'T work
+    
+    print_library();   
+    
+    printf("\n============\n\n");
+    
+    
+    printf("TESTING SONG SEARCH: Search Adele - Skyfall:\n\n");
+    
+    song_node * y = song_search("skyfall", "adele");
+    
+    print_list(y);
+    
+    printf("\n============\n\n");
+    
+    printf("TESTING ARTIST SEARCH: Search ke$ha:\n\n");
+    
+    y = artist_search("ke$ha");
+    
+    print_list(y);
+    
+    printf("\n============\n\n");
+    
+    printf("TESTING PRINT LETTER: Printing t:\n\n");
+    
+    print_letter('t');
+    
+    printf("\n============\n\n");
+    
+    printf("TESTING PRINT ARTIST: Printing adele:\n\n");
+
+    
+
+
+
+    
     /*
     
   printf("\n\nLINKED LIST TESTS\n\n\n");
@@ -118,10 +227,6 @@ int main(){
 
 <<<<<<< HEAD
     */
-=======
-  print_letter("i");
-
->>>>>>> 090c5bc59bf862529eb5a3ae41c8c99999b1f0c6
   return 0;
 
 }
